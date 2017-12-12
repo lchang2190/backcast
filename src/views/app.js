69dd7c3d8c06f5002a236721249fb6206326743c
lchang2190 <lchang2190@gmail.com>
2017-12-11 22:35:20 -0800

    progress!

--- src/views/app.js
+++ src/views/app.js
@@ -2,13 +2,27 @@ var AppView = Backbone.View.extend({
 
   el: '#app',
 
+  // collection: new Videos(exampleVideoData),
+
   initialize: function() {
-    this.videos = new Videos();
+    //this.videos = new Videos(exampleVideoData);    
+    //this.el.$('.list');
+    
+    // var entryView = new VideoListEntryView({ model: video});
+     //var listView = new VideoListView();
+
+    
+    
   },
 
 
   render: function() {
+    console.log(this.collection.models[0]);
     this.$el.html(this.template());
+    new VideoListView({collection: this.collection}).render();
+    new SearchView({collection: this.collection}).render();
+    new VideoPlayerView({model: this.collection.models[0]}).render();
+    //this.$el.html(this.template());
     return this;
   },
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,5 +1,11 @@
 var VideoListView = Backbone.View.extend({
 
+  el: '.list',
+
+  initialize: function() {
+    //this.collection = new VideoListEntry();
+    this.render();    
+  },
 
   render: function() {
     this.$el.children().detach();
